da6fb88d1dabec381a8982ec3a25c246bd6f2fde
bohdan.hrybach <bohdan.hrybach@gmail.com>
2018-03-31 18:56:36 +0300

    Initial Commit

---
+++ Todoe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoe
+//
+//  Created by Bohdan Hrybach on 3/31/18.
+//  Copyright © 2018 Bohdan Hrybach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
